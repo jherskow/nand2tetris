8d9648b5b9bccfc105a50d57430d2388c71ee80a
ayaj <aya-jbara@mail.huji.ac.il>
2017-12-26 23:37:07 +0200

    SymbolTable ~aya

--- projects/11/VMWriter/SymbolTable.py
+++ projects/11/VMWriter/SymbolTable.py
@@ -7,6 +7,10 @@
 ##########################################################################
 
 
+VAR_KIND = "VAR"
+ARG_KIND = "ARG"
+FIELD_KIND = "FIELD"
+STATIC_KIND = "STATIC"
 
 
 class SymbolTable:
@@ -21,13 +25,21 @@ class SymbolTable:
         """
         Creates a new empty symbol table.
         """
-
+        self.static_counter = 0
+        self.field_counter = 0
+        self.argument_counter = 0
+        self.var_counter = 0
 
     def start_subroutine(self):
         """
         Starts a new subroutine scope (i.e., resets the subroutine’s symbol table).
         """
-
+        self.static_counter = 0
+        self.field_counter = 0
+        self.argument_counter = 0
+        self.var_counter = 0
+        self.class_identifiers = {}
+        self.subroutine_identifiers = {}
 
     def define(self, name, type, kind):
         """
@@ -35,10 +47,34 @@ class SymbolTable:
         STATIC and FIELD identifiers have a class scope, while ARG and VAR identifiers have a subroutine scope.
         """
 
+        if kind == VAR_KIND:
+            self.var_counter += 1
+            self.subroutine_identifiers[name] = [type, kind, self.var_counter]
+        elif kind == ARG_KIND:
+            self.argument_counter += 1
+            self.subroutine_identifiers[name] = [type, kind, self.argument_counter]
+        elif kind == STATIC_KIND:
+            self.static_counter += 1
+            self.class_identifiers[name] = [type, kind, self.static_counter]
+        elif kind == FIELD_KIND:
+            self.field_counter += 1
+            self.class_identifiers[name] = [type, kind, self.field_counter]
+
     def var_count(self, kind):
         """
         Returns the number of variables of the given kind already defined in the current scope.
+        kind = (STATIC ,FIELD , ARG ,or VAR)
         """
+        if kind == STATIC_KIND:
+            return self.static_counter
+        elif kind == FIELD_KIND:
+            return self.field_counter
+        elif kind == ARG_KIND:
+            return self.argument_counter
+        elif kind == VAR_KIND:
+            return self.var_counter
+        else:
+            return -1
 
     def kind_of(self, name):
         """
@@ -46,9 +82,27 @@ class SymbolTable:
         If the identifier is unknown in the current scope, returns NONE .
         """
 
+        if self.class_identifiers.get(name) != None:
+            return self.class_identifiers.get(name)[1]
+        elif self.subroutine_identifiers.get(name) != None:
+            return self.subroutine_identifiers.get(name)[1]
+        else:
+            return None
+
     def type_of(self, name):
         """Returns the type of the named identifier in the current scope."""
+        if self.class_identifiers.get(name) != None:
+            return self.class_identifiers.get(name)[0]
+        elif self.subroutine_identifiers.get(name) != None:
+            return self.subroutine_identifiers.get(name)[0]
+        else:
+            return None
 
-
-    def index_of(self,name):
+    def index_of(self, name):
         """Returns the index assigned to the named identifier."""
+        if self.class_identifiers.get(name) != None:
+            return self.class_identifiers.get(name)[2]
+        elif self.subroutine_identifiers.get(name) != None:
+            return self.subroutine_identifiers.get(name)[2]
+        else:
+            return -1

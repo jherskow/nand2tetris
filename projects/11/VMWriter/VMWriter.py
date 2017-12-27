c2917a05e28431093511a0d89e7d4cbbf278af73
ayaj <aya-jbara@mail.huji.ac.il>
2017-12-27 15:57:54 +0200

    proj 11 init commit

--- projects/11/VMWriter/VMWriter.py
+++ projects/11/VMWriter/VMWriter.py
@@ -19,39 +19,39 @@ class VMWriter:
 
     def write_push(self, segment, index):
         """Writes a VM push command."""
-        self.output_file.write("push " + str(segment) + " " + str(index))
+        self.output_file.write("push " + str(segment) + " " + str(index) + "\n")
 
     def write_pop(self, segment, index):
         """Writes a VM pop command."""
-        self.output_file.write("push " + str(segment) + " " + str(index))
+        self.output_file.write("push " + str(segment) + " " + str(index) + "\n")
 
     def write_arithmetic(self, command):
         """Writes a VM arithmetic command."""
-        self.output_file.write(command)
+        self.output_file.write(command + "\n")
 
     def write_label(self, label):
         """Writes a VM label command."""
-        self.output_file.write("label " + label)
+        self.output_file.write("label " + label + "\n")
 
     def write_goto(self, label):
         """Writes a VM goto command."""
-        self.output_file.write("goto " + label)
+        self.output_file.write("goto " + label + "\n")
 
     def write_if(self, label):
         """Writes a VM if-goto command."""
-        self.output_file.write("if-goto " + label)
+        self.output_file.write("if-goto " + label + "\n")
 
     def write_call(self, name, n_args):
         """Writes a VM call command."""
-        self.output_file.write("call " + str(name) + " " + str(n_args))
+        self.output_file.write("call " + str(name) + " " + str(n_args) + "\n")
 
     def write_function(self, name, n_locals):
         """Writes a VM function command."""
-        self.output_file.write("function " + str(name) + " " + str(n_locals))
+        self.output_file.write("function " + str(name) + " " + str(n_locals) + "\n")
 
     def write_return(self):
         """Writes a VM return command."""
-        self.output_file.write("return")
+        self.output_file.write("return" + "\n")
 
     def close(self):
         """Closes the output file."""
